18a78b4eb10f3165c2a7294ccfa4262e4680a378
Safraz <safraz.rampersaud@gmail.com>
2022-08-24 21:59:59 -0400

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,78 +1,17 @@
 from card import *
 import random
+from dataclasses import dataclass, field
 
 
+def _make():
+    init_deck = [Card(suite=suite, value=value) for suite in CardDefaults().suites for value in CardDefaults().values]
+    random.shuffle(init_deck)
+    return init_deck
+
+
+@dataclass
 class Deck:
-    def __init__(self):
-        self.__is_deck_new = True
-        self.__is_deck_mod = False
-        self.__init_deck = self.initialize()
-        self.__deck = self.__init_deck
+    deck: List[Card] = field(default_factory=_make)
 
-    def initialize(self):
-        if self.__is_deck_new:
-            hearts = [Heart(i) for i in range(2, 11)]
-            clubs = [Club(i) for i in range(2, 11)]
-            spades = [Spade(i) for i in range(2, 11)]
-            diamonds = [Diamond(i) for i in range(2, 11)]
-
-            kings = [
-                King(SUIT.CLUB),
-                King(SUIT.HEART),
-                King(SUIT.SPADE),
-                King(SUIT.DIAMOND),
-            ]
-
-            queens = [
-                Queen(SUIT.CLUB),
-                Queen(SUIT.HEART),
-                Queen(SUIT.SPADE),
-                Queen(SUIT.DIAMOND),
-            ]
-
-            jacks = [
-                Jack(SUIT.CLUB),
-                Jack(SUIT.HEART),
-                Jack(SUIT.SPADE),
-                Jack(SUIT.DIAMOND),
-            ]
-
-            aces = [
-                Ace(SUIT.CLUB),
-                Ace(SUIT.HEART),
-                Ace(SUIT.SPADE),
-                Ace(SUIT.DIAMOND),
-            ]
-
-            deck = sum([
-                diamonds,
-                spades,
-                clubs,
-                hearts,
-                aces,
-                kings,
-                queens,
-                jacks
-            ], [])
-
-            random.shuffle(deck)
-
-            return deck
-
-    def print_deck(self):
-        for i in self.__init_deck:
-            print(i)
-
-    def shuffle(self):
-        if self.__is_deck_new:
-            random.shuffle(self.__init_deck)
-        if self.__is_deck_mod:
-            random.shuffle(self.__deck)
-
-    @property
-    def deck(self):
-        return self.__deck
-
-    @property
-    def count(self):
-        return len(self.__deck)
+    def __repr__(self):
+        return '|'.join(f'''{c}''' for c in self.deck)
