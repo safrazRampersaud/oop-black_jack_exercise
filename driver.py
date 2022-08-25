a2b060d00af7c8beae88641d2673166a153a4516
Safraz <safraz.rampersaud@gmail.com>
2022-08-24 22:00:22 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -3,14 +3,13 @@ from game import *
 
 if __name__ == "__main__":
 
+    d = Dealer(0)
+
     p1 = Player(1)
     p2 = Player(2)
     p3 = Player(3)
     p4 = Player(4)
 
-    d = Dealer()
-
     game = Game(dealer=d, players=[p1, p2, p3, p4])
     game.init_deal()
     game.play()
-
